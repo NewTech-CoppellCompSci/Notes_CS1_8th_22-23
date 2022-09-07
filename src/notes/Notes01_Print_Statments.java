2b85ebd0783f60a4b8e65d8f68b1a64dd54a89cb
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-09-07 15:06:51 -0500

    Create Notes01_Print_Statments.java

---
+++ src/notes/Notes01_Print_Statments.java
@@ -0,0 +1,35 @@
+package notes;
+
+public class Notes01_Print_Statments {
+
+	public static void main(String[] args) {
+		
+		//print hello to the screen
+		System.out.println("Hello");
+		
+		//print 2 more times
+		System.out.print("Hello");
+		System.out.println();
+		System.out.print("Hello\n");
+		System.out.println("Hello");
+		
+		/*
+		 * Escape Sequence
+		 * 
+		 * for buttons that don't have characters
+		 * 
+		 * \n = enter key
+		 * \t = tab key
+		 * \\ = prints a backslash
+		 * \" = prints a quote
+		 */
+		
+		
+		System.out.println("/\\");
+		System.out.println("\"The Giver\"");
+		
+		System.out.println("🥲");
+		
+	}
+	
+}
